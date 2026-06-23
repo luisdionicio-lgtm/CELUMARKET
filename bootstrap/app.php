4a84f0a05d8c83751f912061bcaa011de0119236
luisdionicio <luis.dionicio@tecsup.edu.pe>
2026-06-22 22:20:05 -0500

    Force HTTPS asset URLs on Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            at: '*',
+            headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR
+                | \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST
+                | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT
+                | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
+                | \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
         // Registrar alias para middlewares personalizados
         $middleware->alias([
             'is_admin' => \App\Http\Middleware\IsAdmin::class,
